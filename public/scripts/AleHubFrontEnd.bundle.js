a7e38c499bf59faac28c24c878dcf97aa9a6432a
Dman89 <upnvapesd@gmail.com>
2016-10-24 10:50:40 -0700

    Img Location for Static Page Fixed

--- public/scripts/AleHubFrontEnd.bundle.js
+++ public/scripts/AleHubFrontEnd.bundle.js
@@ -27327,6 +27327,7 @@ webpackJsonp([0,1],[
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+	//TODO: Change Location
 	var Banner = function (_React$Component) {
 	  _inherits(Banner, _React$Component);
 
@@ -27342,7 +27343,7 @@ webpackJsonp([0,1],[
 	      return _react2.default.createElement(
 	        "div",
 	        null,
-	        _react2.default.createElement("img", { src: "img/banner.jpg", alt: "" }),
+	        _react2.default.createElement("img", { src: "public/img/banner.jpg", alt: "" }),
 	        _react2.default.createElement(
 	          "h1",
 	          { className: "hidden-xs" },
